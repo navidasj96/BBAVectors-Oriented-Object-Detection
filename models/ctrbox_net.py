9a45f364aaa6d05a82e12e1b3a2e6e5db74b5540
navidasj96 <89703564+navidasj96@users.noreply.github.com>
2022-10-31 09:58:07 +0330

    Update ctrbox_net.py

--- models/ctrbox_net.py
+++ models/ctrbox_net.py
@@ -1,33 +1,32 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 import torch
 from .model_parts import CombinationModule
 from . import resnet
+from .cem import CEM
+
+
 
 class CTRBOX(nn.Module):
-    def __init__(self, heads, pretrained, down_ratio, final_kernel, head_conv):
+    def __init__(self, heads, pretrained, head_conv):
         super(CTRBOX, self).__init__()
-        channels = [3, 64, 256, 512, 1024, 2048]
-        assert down_ratio in [2, 4, 8, 16]
-        self.l1 = int(np.log2(down_ratio))
-        self.base_network = resnet.resnet101(pretrained=pretrained)
-        self.dec_c2 = CombinationModule(512, 256, batch_norm=True)
-        self.dec_c3 = CombinationModule(1024, 512, batch_norm=True)
-        self.dec_c4 = CombinationModule(2048, 1024, batch_norm=True)
+        
+        self.base_network = resnet.res2net50(pretrained=pretrained)
         self.heads = heads
 
         for head in self.heads:
             classes = self.heads[head]
             if head == 'wh':
-                fc = nn.Sequential(nn.Conv2d(channels[self.l1], head_conv, kernel_size=3, padding=1, bias=True),
+                fc = nn.Sequential(nn.Conv2d(320, head_conv, kernel_size=3, padding='same', bias=True),
                                 #    nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
                                    nn.ReLU(inplace=True),
                                    nn.Conv2d(head_conv, classes, kernel_size=3, padding=1, bias=True))
             else:
-                fc = nn.Sequential(nn.Conv2d(channels[self.l1], head_conv, kernel_size=3, padding=1, bias=True),
+                fc = nn.Sequential(nn.Conv2d(320, head_conv, kernel_size=3, padding='same', bias=True),
                                 #    nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
                                    nn.ReLU(inplace=True),
-                                   nn.Conv2d(head_conv, classes, kernel_size=final_kernel, stride=1, padding=final_kernel // 2, bias=True))
+                                   nn.Conv2d(head_conv, classes, kernel_size=3, stride=1, padding='same', bias=True))
             if 'hm' in head:
                 fc[-1].bias.data.fill_(-2.19)
             else:
@@ -49,13 +48,10 @@ class CTRBOX(nn.Module):
         #     temp = x[1][0,idx,:,:]
         #     temp = temp.data.cpu().numpy()
         #     plt.imsave(os.path.join('dilation', '{}.png'.format(idx)), temp)
-        c4_combine = self.dec_c4(x[-1], x[-2])
-        c3_combine = self.dec_c3(c4_combine, x[-3])
-        c2_combine = self.dec_c2(c3_combine, x[-4])
+        final=CEM(x)
 
         dec_dict = {}
         for head in self.heads:
-            dec_dict[head] = self.__getattr__(head)(c2_combine)
-            if 'hm' in head or 'cls' in head:
-                dec_dict[head] = torch.sigmoid(dec_dict[head])
+            dec_dict[head] = self.__getattr__(head)(final)
+            
         return dec_dict
